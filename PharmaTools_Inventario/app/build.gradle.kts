4529058a8566a2206b29e0e2c13702882a405813
Jhonnysga <jhonnysga@users.noreply.github.com>
2026-08-05 20:56:03 -0400

    Subir compileSdk/targetSdk a 34 por requisitos de AndroidX

--- PharmaTools_Inventario/app/build.gradle.kts
+++ PharmaTools_Inventario/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.pharmatools.inventario"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.pharmatools.inventario"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
